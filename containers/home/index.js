c0ba0e4bb1d3746f834cf79332bb1e36f62e8feb
Facundo Brahim <facundo.e.brahim@gmail.com>
2021-05-04 00:03:26 -0300

    Page Home

---
+++ containers/home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {HomeContainer} from './styled'
+
+function Home() {
+  return (
+    <HomeContainer>
+        <h1>Hola</h1>
+    </HomeContainer>
+  )
+}
+
+export default Home;
---
+++ containers/home/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const HomeContainer = styled.div`
+    background-color: red;
+    width: 100%;
+    height: 100vh;
+`;
+
+export { HomeContainer };
