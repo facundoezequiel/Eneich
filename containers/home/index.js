cfa997302da76d2af50f208396b14e13ec3243ff
Facundo Brahim <facundo.e.brahim@gmail.com>
2021-05-05 00:03:06 -0300

    Header

--- containers/home/index.js
+++ containers/home/index.js
@@ -1,12 +1,53 @@
-import React from 'react'
-import {HomeContainer} from './styled'
+import React from "react";
+import Head from "next/head";
+import Ticker from 'react-ticker'
+import { HomeContainer } from "./styled";
 
 function Home() {
   return (
     <HomeContainer>
-        <h1>Hola</h1>
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/pangea/PangeaTextTrial-Light.woff"
+          as="font"
+          crossOrigin=""
+        />
+      </Head>
+      {/* Header */}
+      <div id="header">
+        <div>
+          <img src="images/logos/eneich.svg" alt="Eneich Logo" />
+        </div>
+        <div>
+          <Ticker mode="smooth" offset={0}>
+            {({ index }) => (
+              <>
+              <div className="telepronterDiv">
+                <p>17 de octubre EN VIVO - Aeroposta La Falda</p>
+              </div>
+              <div className="telepronterDiv">
+                <p>Kali Mist - Gandini ft. Eneich. Live now on Spotify</p>
+              </div>
+            </>
+            )}
+          </Ticker>
+        </div>
+        <div id="shopDiv">
+          <a>SHOP</a>
+        </div>
+      </div>
+      {/* Introduction */}
+      <div
+        id="introduction"
+        style={{
+          backgroundImage: `url(${"/images/Background1920.jpg"})`,
+        }}
+      >
+        Hola
+      </div>
     </HomeContainer>
-  )
+  );
 }
 
 export default Home;
--- containers/home/styled.js
+++ containers/home/styled.js
@@ -1,9 +1,84 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const HomeContainer = styled.div`
-    background-color: red;
+const HomeContainer = styled.section`
+  width: 100%;
+
+  // Fonts
+  @font-face {
+    font-family: "PangeaTextTrial";
+    src: url("/fonts/pangea/PangeaTextTrial-Light.woff");
+    font-style: normal;
+    font-weight: 100;
+    font-display: swap;
+  }
+
+  // Header
+  #header {
+    backdrop-filter: blur(30px) brightness(1.08);
+    -webkit-backdrop-filter: blur(30px) brightness(1.08);
+    min-height: 69px;
+    position: fixed;
     width: 100%;
-    height: 100vh;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    // border-bottom: 1px solid #6a615a;
+
+    div {
+      width: 33.33%;
+    }
+
+    img {
+      width: 25%;
+      max-width: 134.89px;
+      margin-left: 15%;
+    }
+
+    .ticker {
+      width: 100%;
+      display: flex;
+
+      div {
+        width: 120%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        align-content: center;
+      }
+
+      .telepronterDiv {
+        width: 69% !important;
+        margin: 0% 7% 0% 7%;
+      }
+    }
+
+    p {
+      color: #141414;
+      width: 100%;
+      height: 100%;
+      text-rendering: optimizeLegibility;
+      font-family: 'Pangea', sans-serif;
+      font-size: 0.9em;
+    }
+
+    #shopDiv {
+      display: flex;
+      justify-content: flex-end;
+
+      a {
+        margin-right: 15%;
+      }
+    }
+  }
+
+  // Introduction
+  #introduction {
+    width: 100%;
+    height: 100vh !important;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
 `;
 
 export { HomeContainer };
