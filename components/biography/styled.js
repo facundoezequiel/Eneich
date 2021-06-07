012ff612aed6a940ef0d98d7e12c924c7cf495a7
Facundo Brahim <facundo.e.brahim@gmail.com>
2021-06-07 17:45:04 -0300

    Font Fix

--- components/biography/styled.js
+++ components/biography/styled.js
@@ -31,7 +31,7 @@ const BiographyContainer = styled.div`
   #textContainer h2 {
     color: #e52b08;
     text-rendering: optimizeLegibility;
-    font-family: "MT Dalat Sans", serif;
+    font-family: "MTDalatSans", serif;
     font-style: normal;
     font-weight: normal;
     font-size: 50px;
