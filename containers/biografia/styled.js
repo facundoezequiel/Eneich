63392573944e250ebb8ac30b22ef2ee282588212
Facundo Brahim <facundo.e.brahim@gmail.com>
2021-07-19 16:52:39 -0300

    Biography Responsive

--- containers/biografia/styled.js
+++ containers/biografia/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { breakpoints } from "../../constants/media";
 
 // Biography Esp Eng Container
 const BioespengContainer = styled.div`
@@ -18,14 +19,15 @@ const BioespengContainer = styled.div`
   #titleContainer {
     width: 100%;
     background-color: var(--nhdarkgray);
-    padding: 3% 0% 2% 0%;
+    padding: 80px 0px 60px 0px;
   }
 
   // Title Container
   #titleContainer div {
-    width: 30%;
+    width: 100%;
     margin: 0 auto;
-    max-width: 1344px;
+    min-width: 768px;
+    max-width: 768px;
   }
 
   // Title
@@ -35,7 +37,7 @@ const BioespengContainer = styled.div`
     font-family: "MT Dalat Sans", serif;
     font-style: normal;
     font-weight: normal;
-    font-size: 50px;
+    font-size: 3.125em;
     letter-spacing: 0.05em;
     text-align: left;
     padding: 30px 0px 20px 0px;
@@ -59,17 +61,18 @@ const BioespengContainer = styled.div`
   // Text Container
   #pContainer {
     width: 100%;
-    padding: 3% 0% 5% 0%;
+    padding: 60px 0px 100px 0px;
     background-color: var(--nhwhite);;
     z-index: 98;
   }
 
   // Text
   #pContainer p {
-    width: 30%;
+    width: 100%;
     margin: 0 auto;
-    max-width: 1344px;
-    font-size: 1.2em;
+    min-width: 768px;
+    max-width: 768px;
+    font-size: 1.3em;
     line-height: 35px;
     font-family: "Lora", serif;
     text-align: left;
@@ -153,6 +156,194 @@ const BioespengContainer = styled.div`
     );
     width: 100%;
     height: 5px;
+  }
+
+  /* ----------- DESKTOP RESPONSIVE ----------- */
+
+  //1366px Window -----------
+  //Estandar
+  @media (max-width: ${breakpoints.window1366}) {
+    // Title Container
+    #titleContainer div {
+      min-width: 700px;
+      max-width: 700px;
+    }
+
+    // Text
+    #pContainer p {
+      min-width: 700px;
+      max-width: 700px;
+      font-size: 1.2em;
+    }
+  }
+
+  /* ----------- TABLET RESPONSIVE ----------- */
+
+  //1024px Tablet -----------
+  //iPad Pro
+  @media (max-width: ${breakpoints.tablet1024}) {
+    // Title Container
+    #titleContainer {
+      padding: 105px 0px 60px 0px;
+    }
+
+    // Title
+    #titleContainer h2 {
+      font-size: 2.5em;
+    }
+
+    // Subtitle
+    #titleContainer p {
+      font-size: 0.813em !important;
+    }
+
+    // Back Button Text
+    #backButton span {
+      font-size: 0.813em !important;
+    }
+  }
+
+  //830px Tablet -----------
+  @media (max-width: ${breakpoints.tablet830}) {
+    // Title Container
+    #titleContainer div {
+      min-width: 600px;
+      max-width: 600px;
+    }
+
+    // Text
+    #pContainer p {
+      min-width: 600px;
+      max-width: 600px;
+      font-size: 1.1em;
+      line-height: 32px;
+    }
+  }
+
+  //768px Tablet -----------
+  //iPad
+  @media (max-width: ${breakpoints.tablet768}) {
+    // Title Container
+    #titleContainer div {
+      min-width: 500px;
+      max-width: 500px;
+    }
+
+    // Text
+    #pContainer p {
+      min-width: 500px;
+      max-width: 500px;
+    }
+  }
+
+  /* ----------- MOBILE RESPONSIVE ----------- */
+
+  //651px Mobile -----------
+  @media (max-width: ${breakpoints.mobile651}) {
+    // Title Container
+    #titleContainer div {
+      min-width: 80%;
+      max-width: 80%;
+      width: 80%;
+    }
+
+    // Text
+    #pContainer p {
+      width: 80%;
+      min-width: 80%;
+      max-width: 80%;
+      font-size: 1.4em;
+      line-height: 40px;
+    }
+
+    // Title
+    #titleContainer h2 {
+      font-size: 3.125em;
+    }
+
+    // Subtitle
+    #titleContainer p {
+      font-size: 0.9em !important;
+      letter-spacing: 2px;
+    }
+
+    // Back Button Text
+    #backButton span {
+      font-size: 0.9em !important;
+    }
+  }
+
+  //550px Mobile -----------
+  @media (max-width: ${breakpoints.mobile550}) {
+    // Title
+    #titleContainer h2 {
+      font-size: 2.35em;
+    }
+
+    // Text
+    #pContainer p {
+      font-size: 1.1em;
+      line-height: 35px;
+    }
+
+    // Subtitle
+    #titleContainer p {
+      font-size: 0.8em !important;
+    }
+
+    // Back Button Text
+    #backButton span {
+      font-size: 0.8em !important;
+    }
+  }
+
+  //375px Mobile -----------
+  //iPhone X
+  @media (max-width: ${breakpoints.mobile375}) {
+    // Title
+    #titleContainer h2 {
+      font-size: 2.2em;
+    }
+  }
+
+  //360px Mobile -----------
+  //Moto G4
+  @media (max-width: ${breakpoints.mobile375}) {
+    // Title
+    #titleContainer h2 {
+      font-size: 2em;
+    }
+
+    // Subtitle
+    #titleContainer p {
+      font-size: 0.75em !important;
+      letter-spacing: 1px;
+    }
+  }
+
+  //280px Mobile -----------
+  //Galaxy Fold
+  @media (max-width: ${breakpoints.mobile280}) {
+    // Title
+    #titleContainer h2 {
+      font-size: 1.7em;
+    }
+
+    // Text
+    #pContainer p {
+      font-size: 1em;
+      line-height: 27px;
+    }
+
+    // Subtitle
+    #titleContainer p {
+      font-size: 0.65em !important;
+    }
+
+    // Back Button Text
+    #backButton span {
+      font-size: 0.7em !important;
+    }
   }
 `;
 
